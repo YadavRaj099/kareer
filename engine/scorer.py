647c7f176d19eb79c70801169d82b16cb99cdb54
Raj Yadav <Yadavrajanil2610@gmail.com>
2026-03-13 15:59:06 +0530

    Update scorer.py

--- engine/scorer.py
+++ engine/scorer.py
@@ -1,17 +1,55 @@
 def score_career(career, user, rules):
 
+    weights = rules["weights"]
+
     score = 0
+    max_score = sum(weights.values())
 
-    tags = career.get("tags", [])
-    risk = career.get("risk", "")
+    career_tags = [t.lower() for t in career.get("tags", [])]
 
-    if user["subjects"].lower() in [t.lower() for t in tags]:
-        score += rules["subjects_match"]
+    # SUBJECT MATCH
+    user_subjects = [s.lower() for s in user.get("subjects", [])]
 
-    if user["work_type"].lower() in [t.lower() for t in tags]:
-        score += rules["work_type_match"]
+    if any(sub in career_tags for sub in user_subjects):
+        score += weights["subjects_match"]
 
-    if user["risk"].lower() == risk.lower():
-        score += rules["risk_match"]
+    # INTEREST MATCH
+    interest = user.get("work_type", "").lower()
 
-    return score
+    if interest in career_tags:
+        score += weights["interest_match"]
+
+    # WORK STYLE
+    team_style = user.get("team_style", "").lower()
+
+    if team_style in career_tags:
+        score += weights["work_style_match"]
+
+    # RISK MATCH
+    career_risk = career.get("risk", "").lower()
+    user_risk = user.get("risk", "").lower()
+
+    if career_risk == user_risk:
+        score += weights["risk_match"]
+
+    # EDUCATION YEARS
+    education_pref = user.get("education_commitment", "")
+
+    if education_pref == career.get("education_years"):
+        score += weights["education_match"]
+
+    # MOTIVATION
+    motivation = user.get("motivation", "").lower()
+
+    if motivation in career_tags:
+        score += weights["motivation_match"]
+
+    # LIFESTYLE
+    lifestyle = user.get("lifestyle", "").lower()
+
+    if lifestyle in career_tags:
+        score += weights["lifestyle_match"]
+
+    percentage = round((score / max_score) * 100, 2)
+
+    return percentage
