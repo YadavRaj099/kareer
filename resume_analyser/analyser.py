aaf0cb5b343646dbacd61e8b70000e71ac01fd35
Raj Yadav <Yadavrajanil2610@gmail.com>
2026-03-16 00:12:30 +0530

    Update analyser.py

--- resume_analyser/analyser.py
+++ resume_analyser/analyser.py
@@ -7,8 +7,8 @@ from functools import lru_cache
 from datetime import datetime
 
 from resume_analyser.parser import parse_resume
-from engine.resume_analyzer.skill_extractor import extract_skills
-from engine.resume_analyzer.resume_scorer import score_resume
+from resume_analyzer.skill_extractor import extract_skills
+from resume_analyzer.resume_scorer import score_resume
 from resume_analyzer.role_skills import (
     get_role_skills,
     list_available_roles
